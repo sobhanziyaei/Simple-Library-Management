5d5bd88a056e9ddba6a1fdd8c7c6f1e87a3896ff
Sobhan <ziaey@gmail.com>
2025-02-05 08:43:53 +0330

    create api config for base url

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes } from '../app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { API_BASE_URL } from './api.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    { provide: API_BASE_URL, useValue: 'http://localhost:3000/api' },
   ]
 };
